96dc5b33c56edf3e9ccd5ce5e32fe3187bf14961
Harshank Bansal <harshank.bansal@joinmosaic.com>
2025-10-24 13:40:50 +0530

    some metadata for agentforce agent, however we have to create user manually

--- scripts/pwsh/deploy-agentforce-metadata.ps1
+++ scripts/pwsh/deploy-agentforce-metadata.ps1
@@ -4,25 +4,26 @@ param (
 
 $ErrorActionPreference = "Stop"
 
-sf project retrieve start `
+#When using API Version >=64
+sf project deploy start `
+    --ignore-conflicts `
     --target-org $TargetOrg `
+    --metadata Flow `
     --metadata Bot `
     --metadata BotVersion `
     --metadata GenAiPlannerBundle `
     --metadata GenAiPlugin `
     --metadata GenAiFunction `
-    --metadata PermissionSetGroup
     --metadata PermissionSet:sfdc_aiplanner_service_permset `
     --metadata PermissionSet:Agentforce_Service_Agent_Permissions `
     --metadata PermissionSet:sfdc_chatbot_service_permset
 
-# sf project retrieve start `
+#Using API Version < 64
+# sf project deploy start `
 #     --target-org $TargetOrg `
 #     --metadata Bot `
 #     --metadata BotVersion `
 #     --metadata GenAiPlanner `
 #     --metadata GenAiPlugin `
 #     --metadata GenAiFunction `
-#     --metadata PermissionSet:sfdc_aiplanner_service_permset `
-#     --metadata PermissionSet:Agentforce_Service_Agent_Permissions `
-#     --metadata PermissionSet:sfdc_chatbot_service_permset
+#     --metadata PermissionSet:Agentforce_Service_Agent_Permissions 
